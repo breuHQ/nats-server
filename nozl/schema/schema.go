88c440ac42377fa979dfdd7c52f58ab973d04ed1
Taimoor Mirza <taimoor@breu.io>
2023-05-02 12:28:51 +0500

    feat(schema): Add schema related folder

---
+++ nozl/schema/schema.go
@@ -0,0 +1,30 @@
+package schema
+
+import (
+	// "github.com/getkin/kin-openapi/openapi3"
+	// "github.com/nats-io/nats-server/v2/nozl/shared"
+)
+
+func ParseOpenApiV3Schema() {
+	// operationId -> path -> methods[] -> methodDetails
+	// {
+	// 	"operation_id": {
+	// 		"path": {
+	// 			"methods": {
+	// 				"GET": {}
+	// 				"POST": {}
+	// 			}
+	// 		}
+	// 	}
+	// }
+	// doc, err := openapi3.NewLoader().LoadFromFile("../../twilio_api_v2010.json")
+	
+	// if err != nil {
+	// 	shared.Logger.Error(err.Error())
+	// }
+	// var dict map[string]interface{}
+
+	// for pathKey, pathValue := range doc.Paths {
+
+	// }
+}
