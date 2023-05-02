7062a1b96f6e0a0613a624c27f242ca60e66c49b
Taimoor Mirza <taimoor@breu.io>
2023-05-02 16:10:56 +0500

    refactor(schema): Consume Open API parser method in http handler

---
+++ nozl/schema/schema_handler.go
@@ -0,0 +1,45 @@
+package schema
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func UploadOpenApiSpecHandler(ctx echo.Context) error {
+	file, err := ctx.FormFile("file")
+	if err != nil {
+		return ctx.JSON(http.StatusBadRequest, echo.Map{
+			"message": "File Upload Error",
+		})
+	}
+
+	openApiFile, err := file.Open()
+
+	if err != nil {
+		return ctx.JSON(http.StatusExpectationFailed, echo.Map{
+			"message": "File Opening Error",
+		})
+	}
+	defer openApiFile.Close()
+
+	buf := bytes.NewBuffer(nil)
+
+	if _, err := io.Copy(buf, openApiFile); err != nil {
+		return ctx.JSON(http.StatusInternalServerError, echo.Map{
+			"message": "Error in parsing file",
+		})
+	}
+
+	if err = ParseOpenApiV3Schema(buf.Bytes()); err != nil {
+		return ctx.JSON(http.StatusInternalServerError, echo.Map{
+			"message": "Error in parsing file",
+		})
+	}
+
+	return ctx.JSON(http.StatusOK, echo.Map{
+		"message": "File uploaded successfully",
+	})
+}
