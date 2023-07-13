3af688d5e61648d8b7a39ee19f77ab205775af3c
Mahad Akhter <119653090+mahad-akhter@users.noreply.github.com>
2023-07-13 13:07:09 +0500

    fix(service): Add auth details to service constructor (#23)

--- nozl/service/service.go
+++ nozl/service/service.go
@@ -5,28 +5,29 @@ import (
 )
 
 const (
-	Twilio string = "Twilio"
+	Twilio   string = "Twilio"
 	SendGrid string = "SendGrid"
-	Vonage string = "Vonage"
-	Custom string = "Custom"
+	Vonage   string = "Vonage"
+	Custom   string = "Custom"
 )
 
 type (
 	// TODO: Fix JSON validation issues
 	Service struct {
-		ID         string `json:"id"`
-		Name       string `json:"name"`
-		FilterOn   string `json:"filter_on"`
+		ID          string            `json:"id"`
+		Name        string            `json:"name"`
+		FilterOn    string            `json:"filter_on"`
 		AuthDetails map[string]string `json:"auth_details"`
-		Category   string `json:"category"`
+		Category    string            `json:"category"`
 	}
 )
 
 func NewService(name string, authDetails map[string]string, filterOn string, category string) *Service {
 	return &Service{
-		ID:         uuid.New().String(),
-		Name:       name,
-		FilterOn:   filterOn,
-		Category:   category,
+		ID:          uuid.New().String(),
+		Name:        name,
+		FilterOn:    filterOn,
+		AuthDetails: authDetails,
+		Category:    category,
 	}
 }
