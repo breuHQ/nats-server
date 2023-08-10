8fbf4577e1e53e7198b1823fa7631e42d6024bfe
BehrozKarim <behrozbuzdar@gmail.com>
2023-08-10 14:16:30 +0500

    bug(FilterLimit): Logging the error

--- nozl/core/core_handler.go
+++ nozl/core/core_handler.go
@@ -149,6 +149,7 @@ func UpdateCurrFilterConf(limit int) error {
 	allKey, err := kv.Keys()
 	// TODO: Return nil only if there are no keys in the bucket otherwise return err
 	if err != nil {
+		shared.Logger.Error(err.Error())
 		return nil
 	}
 
